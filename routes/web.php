ff47eca2c93959b7eba974ee48db727c5b377c12
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-09-18 05:31:21 +0800

    configuting route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Rout::get('/{any}', function() {
+    return file_get_contents(public_path('index.html'));
 });
