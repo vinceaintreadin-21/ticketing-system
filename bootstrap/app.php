09f2ad790a68c0db7285797d86c3fc70a3d2292b
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-09-24 17:32:04 +0800

    configuring bootstrap via app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class,);
+        $middleware->alias(['role'=>\App\Http\Middleware\RoleMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
